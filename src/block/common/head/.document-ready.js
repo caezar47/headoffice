2fbefd45cac5c821726494f2d70ce09911e40ff0
caezar47 <iss47@ya.ru>
2021-02-05 08:36:25 +0300

    Добавил линию

--- src/block/common/head/.document-ready.js
+++ src/block/common/head/.document-ready.js
@@ -1,8 +1,11 @@
 $('img').addClass('img-responsive');  
 $('.card__item.is--works-map').on('show.bs.collapse', function () {
 	$('.card__item.is--works-map').collapse('hide');
-	$('.section__section.is--page-works-contacts').removeClass('is--show-card');
-	$('.section__section.is--page-works-contacts').addClass('is--show-card');
-	//$(this).addClass('is--show');
 	$('.section__section.is--page-works-contacts').addClass('is--show');
+});
+$('.card__item.is--works-map').on('shown.bs.collapse', function () {
+	$(this).addClass('is--show-card');
+});
+$('.card__item.is--works-map').on('hidden.bs.collapse', function () {
+	$(this).removeClass('is--show-card');
 });
