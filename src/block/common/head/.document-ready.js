0b134041e1b06450962db91c7a2aef329dcf0606
caezar47 <iss47@ya.ru>
2021-02-15 13:13:19 +0300

    правки

--- src/block/common/head/.document-ready.js
+++ src/block/common/head/.document-ready.js
@@ -1,11 +1,1 @@
 $('img').addClass('img-responsive');  
-$('.card__item.is--works-map').on('show.bs.collapse', function () {
-	$('.card__item.is--works-map').collapse('hide');
-	$('.section__section.is--page-works-contacts').addClass('is--show');
-});
-$('.card__item.is--works-map').on('shown.bs.collapse', function () {
-	$(this).addClass('is--show-card');
-});
-$('.card__item.is--works-map').on('hidden.bs.collapse', function () {
-	$(this).removeClass('is--show-card');
-});
